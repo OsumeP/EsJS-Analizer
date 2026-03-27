7fdb89529d6e16224e74135cc39a00d4a88eee2f
OsumeP <juandavid.patarroyosanchez@gmail.com>
2026-03-27 16:07:58 -0500

    Fixed some bugs related with regex, nums and spread operator

--- LexicAnalizer.py
+++ LexicAnalizer.py
@@ -8,23 +8,33 @@ class LexicAnalizer:
     row: int
     column: int
     comment: bool
+    regex: bool
+    canBe: bool
+    inputCon: str
+    index: int
+    startLexema: int
 
-    def __init__(self):
+    def __init__(self, inputCol: str):
         self.column = 1
         self.row = 1
         self.lexema = ""
         self.comment = False
+        self.regex = False
+        self.inputCon = inputCol
+        self.index = 0
+        self.startLexema = 0
+        self.canBe = True
 
         self.keywords = {
                     #Control Keywords
-                    'capturar', 'caso', 'con', 'continuar', 'crear', 'elegir', 'elegir', 'mientras', 'para', 'retornar', 'sino', 'si', 'constructor', 'eliminar', 'extiende', 'finalmente', 'instanciaDe',
+                    'capturar', 'caso', 'con', 'continuar', 'crear', 'elegir', 'esperar', 'hacer', 'mientras', 'para', 'retornar', 'sino', 'si', 'constructor', 'eliminar', 'extiende', 'finalmente', 'instanciaDe',
                     'intentar', 'lanzar', 'longitud', 'romper', 'simbolo', 'subcad', 'tipoDe', 'vacio', 'ambiente', 'super', 'de', 'en', 'clase', 'const', 'var', 'mut', 'porDefecto', 'funcion',
                     #Language Constants
                     'falso', 'nulo', 'verdadero', 'indefinido', 'Infinito', 'NuN', 
                     #Support Functions
                     'consola', 'Fecha', 'Numero', 'Mate', 'Matriz', 'Arreglo', 'Booleano', 'Cadena', 'Funcion', 
                     #Console Object
-                    'afirmar', 'limpiar', 'listar', 'listarXml', 'error',
+                    'afirmar', 'limpiar', 'listar', 'error',
                     'agrupar', 'info', 'escribir', 'tabla',
                     #String Object
                     'enPosicion', 'caracterEn', 'codigoDeCaracterEn', 'puntoDeCodigoEn', 'concatenar', 'terminaCon', 'desdeCodigoDeCaracter', 'desdePuntoDeCodigo', 'incluye',
@@ -45,7 +55,7 @@ class LexicAnalizer:
         
         self.operations = {"&&": "and", "||": "or", "...": "spread", ".": "period", ",": "comma", ";": "semicolon", ":": "colon", "{": "opening_key", "}": "closing_key", "[": "opening_bra", "]": "closing_bra", 
                            "(": "opening_par", ")": "closing_par", "++": "increment", "--": "decrement", "%=": "mod_assign", "/=": "div_assign", "*=": "times_assign", "-=": "minus_assign", "+=": "plus_assign",
-                           "**=": "power_assign", "+": "plus", "-": "minus", "*": "times", "/": "div", "%": "mod", "==": "equal", "===": "strict_equal", "!=": "neq", "!==": "strict_neq", "<=": "leq", ">=": "geq",
+                           "**=": "power_assign", "+": "plus", "-": "minus", "*": "times", "/": "div", "**": "power", "%": "mod", "==": "equal", "===": "strict_equal", "!=": "neq", "!==": "strict_neq", "<=": "leq", ">=": "geq",
                            ">": "greater", "<": "less", "=": "assign", "=>": "arrow", "!": "not", "?": "ternary", "??": "nulish"}
     
     
@@ -67,39 +77,78 @@ class LexicAnalizer:
             else:
                 return ["id", string]
         #String
-        elif re.fullmatch(r'"([^"\\]|\\.)*"?|\'([^\'\\]|\\.)*\'?', string):
+        elif re.fullmatch(r'"(?:\\.|[^"\\\n])*"?|\'(?:\\.|[^\'\\\n])*\'?', string):
             return ["tkn_str", string[1:len(string) - 1]]
         #Number
-        elif re.fullmatch(r'\d+\.\d+|\d+|\.\d+|\d+\.\d*', string):
+        elif re.fullmatch(r'\d+\.\d+|\d+', string):
             return ["tkn_num", string]
         #Operations
         elif string in self.operations:
             return ["tkn_" + self.operations.get(string)]
-        #Regex
-        elif re.fullmatch(r'/([^/\\]|\\.)+/?', string):
-            return ["tkn_reg", string[1: len(string) - 1]]
         #One line comments and White spaces, tabs, \n, etc.
         elif re.fullmatch(r'\s+', string) or re.fullmatch(r'//[^\n]*', string):
             return []
+        #Regex
+        elif re.fullmatch(r'/(.)+/', string):
+            self.regex = False
+            return ["tkn_reg", string[1: len(string) - 1]]
+        #Regex parcial
+        elif re.fullmatch(r'/(.)+/?', string) and self.canBe:
+            self.regex = True
+            return []
+        elif (string == ".." or re.fullmatch(r'\d+\.\d*', string)) and self.canBe:
+            return []
 
-        None
+        return None
     
     def nextToken(self) -> list[str | int] | None:
 
-        while True:
-            char: str = sys.stdin.read(1)
-            if not char:
+        while self.index < len(self.inputCon) + 1:
+            if self.index == len(self.inputCon):
+                if self.lexema:
+                    token = self.getToken(self.lexema)
+                    if token and len(token) > 0:
+                        result = [*token, self.row, self.column - len(self.lexema)]
+                        self.lexema = ""
+                        return result
                 return None
             
-            flag: bool = self.comment
+            char: str = self.inputCon[self.index]
+            self.index += 1
+            
+            flagComment: bool = self.comment
+            flagRegex: bool = self.regex
             result: list[str | int] | None = None
 
             token: list[str] | None = self.getToken(self.lexema + char)
+            
+            if(char == "\n" and self.regex):
+                self.regex = False
+                self.canBe = False
+                self.column -= len(self.lexema) - 1
+                self.index = self.startLexema + 1
+                self.lexema = "/"
+                continue
+
+            if(len(self.lexema) == 2 and self.lexema[0] == "." and self.lexema[0] == "." and token is None):
+                self.canBe = False
+                self.column -= len(self.lexema) - 1
+                self.index = self.startLexema + 1
+                self.lexema = "."
+                continue
+
+            if(re.fullmatch(r"\d+\.",self.lexema) and not (char >= "0" and char <= "9") ):
+                self.canBe = False
+                self.column -= len(self.lexema)
+                self.index = self.startLexema
+                self.lexema = ""
+                continue
 
             if(token is None):
                 token = self.getToken(self.lexema)
                 lgthLexema: int = len(self.lexema)
                 self.lexema = char
+                self.startLexema = self.index - 1
 
                 if(token is None):
                     return [self.row, self.column - lgthLexema]
@@ -108,7 +157,11 @@ class LexicAnalizer:
                     result = [*token, self.row, self.column - lgthLexema]
             else:
                 self.lexema += char
-                if(flag and not self.comment):
+                if(flagComment and not self.comment):
+                    self.lexema = ""
+                if(flagRegex and not self.regex):
+                    lgthLexema: int = len(self.lexema) - 1
+                    result = [*token, self.row, self.column - lgthLexema]
                     self.lexema = ""
 
             if(char == "\n"):
@@ -117,6 +170,7 @@ class LexicAnalizer:
             self.column += 1
 
             if(result is not None):
+                self.canBe = True
                 return result
 
 def printToken(token: list[str | int]):
@@ -131,7 +185,8 @@ def printToken(token: list[str | int]):
             
 
 
-obj: LexicAnalizer = LexicAnalizer()
+obj: LexicAnalizer = LexicAnalizer(sys.stdin.read())
+obj.inputCon += "\n"
 token: list[str | int] | None = obj.nextToken()
 
 while(token is not None):
@@ -140,7 +195,3 @@ while(token is not None):
         break
     printToken(token)
     token = obj.nextToken()
-
-
-obj.nextToken()
-# print(obj.getToken("/* hola\nmundo */"))
